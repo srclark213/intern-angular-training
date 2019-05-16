8bd04dd8c3dea784a9ceaebbe692a43f1b41a0e0
Steve Clark <steve.clark213@gmail.com>
2019-05-15 19:47:32 -0500

    gutted app for start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule }    from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FilmListComponent } from './components/film-list/film-list.component';
-import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FilmListComponent,
-    HomeComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule,
     FormsModule
   ],
   providers: [],
